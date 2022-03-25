3ee7cba832fd8166a430df45b8c1a8fcb1465ec2
Daniel Kozlowski <daniel.kozlowski@vp.pl>
2022-03-25 19:27:46 +0100

    dodanie odcienie

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,7 @@
 let main = document.querySelector('.main');
 let resetBtn = document.querySelector('.reset')
 
+main.addEventListener("mouseover", hoverOver);
 
 function creatGrid(gridSize=8) {
     for (i=1; i<=gridSize*gridSize; i++){
@@ -12,14 +13,23 @@ function creatGrid(gridSize=8) {
 } creatGrid()
 
 
-function addColor(){
-    let pixels = document.querySelectorAll('.pixel')
-    pixels.forEach(pixel => 
-        pixel.addEventListener('mouseover',() =>{
-        pixel.style.backgroundColor = 'red'
-        }
-        ))
-} addColor()
+function hoverOver(event) {
+    if (event.target !== '.main') {
+        event.target.style.backgroundColor = `rgba(60, 12, 50, ${addColor(event.target)}`;
+    }
+}
+
+function addColor(element) {
+    // regex targeted at numbers with digits taken from this StackOverflow comment
+    // https://stackoverflow.com/questions/3751877/how-to-extract-r-g-b-a-values-from-css-color#comment79938383_3752026
+    let alphaTag = Number(window.getComputedStyle(element).backgroundColor.match(/[.?\d]+/g)[3]);
+
+    if (alphaTag <= 1) {
+        return alphaTag += 0.1;
+    }
+
+    return alphaTag;
+}
 
 function clear(){
     main.innerHTML='';
@@ -30,7 +40,7 @@ resetBtn.addEventListener('click',() => {
     if (gridSize<=50){
         clear()
         creatGrid(gridSize)
-        addColor()
+        gaddColor()
     }
     else {
     alert ('50 is the biggest')
