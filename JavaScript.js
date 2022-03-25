249c70e7c401d24faff7ab8fce36cda7ea95f0cb
Daniel Kozlowski <daniel.kozlowski@vp.pl>
2022-03-25 18:22:30 +0100

    Zrobiony caly

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,42 @@
-const main = document.querySelector('.main');
+let main = document.querySelector('.main');
+let resetBtn = document.querySelector('.reset')
 
 
-for (i=1; i<=256; i++){
-    const dupa = document.createElement('div')
-    main.appendChild(dupa);
-    dupa.classList.add('dupa');
-    dupa.textContent='siema'
+function creatGrid(gridSize=8) {
+    for (i=1; i<=gridSize*gridSize; i++){
+     let pixel = document.createElement('div')
+      main.appendChild(pixel);
+      pixel.classList.add('pixel');
+      pixel.style.border='1px solid black'
+}     main.style.gridTemplateColumns = `repeat(${gridSize} , auto)`
+} creatGrid()
+
+
+function addColor(){
+    let pixels = document.querySelectorAll('.pixel')
+    pixels.forEach(pixel => 
+        pixel.addEventListener('mouseover',() =>{
+        pixel.style.backgroundColor = 'red'
+        }
+        ))
+} addColor()
+
+function clear(){
+    main.innerHTML='';
 }
+
+resetBtn.addEventListener('click',() => {
+    gridSize=prompt('Enter the size of the grid',8)
+    if (gridSize<=50){
+        clear()
+        creatGrid(gridSize)
+        addColor()
+    }
+    else {
+    alert ('50 is the biggest')
+    clear()
+    creatGrid()
+    addColor();
+    }
+}
+)
